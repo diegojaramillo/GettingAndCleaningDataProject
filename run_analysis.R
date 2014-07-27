850e929a7afe2919117bee2db9583f35f06e1f48
Diego Jaramillo <Diego@MacBook-Pro-de-Diego.local>
2014-07-27 10:06:42 -0500

    Run Analysis script

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-#Run Analysis script, Diego Jaramillo Celis 10/07/2014
+#Run Analysis script, Diego Jaramillo Celis 07/2014
 
 #This script:
 # 1. Merges the training and the test sets to create one data set.
